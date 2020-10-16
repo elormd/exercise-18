ed124a5393ab801ed383bb7bfd93a3f54e9651c6
elormd <dziks20052@gmail.com>
2020-10-16 13:28:33 +0000

    this is exercise 18

---
+++ eight.py
@@ -0,0 +1,14 @@
+print('enter a number to see if it is a perfect square.')
+number = input()
+number = abs(int(number))
+i = 1
+square = False 
+while i <= number**(0.5):
+    i += 1
+    if i*2== number 
+    print( square = true)
+    break
+    if sqare :
+        print('the square root of',number, 'is', i,'.')
+    else:
+        print('',number, 'is not a perfect sqaure.')
